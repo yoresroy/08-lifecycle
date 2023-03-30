db8befcbb3001d2cef89f0d330986d5348ebe48c
RodrigoMegasys <rodrigo.ruiz@megasys.com.mx>
2023-03-30 17:04:16 -0600

    Git init

---
+++ src/app/components/muestra-nombre/muestra-nombre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-muestra-nombre',
+  templateUrl: './muestra-nombre.component.html',
+  styleUrls: ['./muestra-nombre.component.css']
+})
+export class MuestraNombreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
